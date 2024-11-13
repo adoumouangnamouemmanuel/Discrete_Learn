501f34b4f87e46eb289eeefba661988cfb5edb88
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2024-11-13 10:24:50 +0000

    added constants to make the code clean, updated about, added Ruth's profile picture

---
+++ src/constants/constants.d.ts
@@ -0,0 +1,10 @@
+// constants/constants.d.ts
+export interface TeamMember {
+  name: string;
+  role: string;
+  description: string;
+  email: string;
+  phone: string;
+  linkedin: string;
+  image: string;
+}
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+// constants.js
+// constants/index.js
+import Emma from "../assets/emma.jpg"; // Adjust this path as needed
+import Ruth from "../assets/ruth.jpg"; // Adjust this path as needed
+
+/** @type {import("./constants").TeamMember[]} */
+export const teamMembers = [
+  {
+    name: "Emmanuel Adoum",
+    role: "Founder & Lead Developer",
+    description:
+      "Emmanuel is the visionary behind DiscreteLearn. As a passionate software engineer, he leads the development of the platform, creating an intuitive and scalable user experience for students across the globe.",
+    email: "emmanuel.adoum@ashesi.edu.gh",
+    phone: "+123456789",
+    linkedin: "https://www.linkedin.com/in/ouang-namou-emmanuel-adoum/",
+    image: Emma,
+  },
+  {
+    name: "Ruth Arhin",
+    role: "Product Manager",
+    description:
+      "Ruth ensures that DiscreteLearn is not only functional but also user-friendly. She works closely with students to gather feedback, ensuring the platform meets learners' needs and expectations.",
+    email: "jane.smith@example.com",
+    phone: "+987654321",
+    linkedin: "https://www.linkedin.com/in/ruth-arhin-/",
+    image: Ruth,
+  },
+  {
+    name: "Nary-Ann Adzim",
+    role: "Lead Designer",
+    description:
+      "Mary-Ann designs the user interface of DiscreteLearn, ensuring the platform is visually appealing and easy to navigate. Her goal is to make learning Discrete Mathematics a pleasant and seamless experience.",
+    email: "mark.johnson@example.com",
+    phone: "+1122334455",
+    linkedin: "https://www.linkedin.com/in/mary-ann-adzim/",
+    image: Emma,
+  },
+];
